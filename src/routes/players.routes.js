e590f9e6273e8422159fe89ae3d32871ce9d7385
MBVL-KD <mbvl1967@gmail.com>
2026-03-16 15:35:01 +0100

    Update players.routes.js

--- src/routes/players.routes.js
+++ src/routes/players.routes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { getPlayerProfile, getRecentGames, getPlayerRatings } from "../controllers/players.controller.js";
+import {
+  getPlayerProfile,
+  getRecentGames,
+  getPlayerRatings,
+  getPlayerRatingSnapshot,
+} from "../controllers/players.controller.js";
 
 const router = Router();
 
 router.get("/:userId/profile", getPlayerProfile);
 router.get("/:userId/recent-games", getRecentGames);
 router.get("/:userId/ratings", getPlayerRatings);
+router.get("/:userId/rating-snapshot", getPlayerRatingSnapshot);
 
 export default router;
